aa044b0726b48da6ed44f9dbc1b79a10f8b9529b
sjf630 <95392688+sjf630@users.noreply.github.com>
2023-07-20 14:29:35 -0500

    fix error index.js

--- Develop/index.js
+++ Develop/index.js
@@ -1,6 +1,6 @@
 const inquirer = require('inquirer');
 const fs = require('fs');
-const generateMarkdown = require('./generateMarkdown.js');
+const generateMarkdown = require('./utils/generateMarkdown');
 
 const questions = [
   {
